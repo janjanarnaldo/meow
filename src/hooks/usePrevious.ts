194714c5daba188f0186c7e3c1fa4726379d3232
Arjan <janjanarnaldo@gmail.com>
2020-08-15 19:28:59 +0800

    Remove unused hook

--- src/hooks/usePrevious.ts
+++
@@ -1,9 +0,0 @@
-import { useRef, useEffect } from 'react';
-
-export const usePrevious = (value?: string) => {
-  const ref = useRef(value);
-  useEffect(() => {
-    ref.current = value;
-  });
-  return ref.current;
-};
